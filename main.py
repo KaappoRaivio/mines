59647df100b627e4fb6b3973f9835151c16a8340
NoSocksForYou <kaappo.raivio@gmail.com>
2018-01-24 15:09:17 +0200

    Added the AmountOfNeighBors method.

--- main.py
+++ main.py
@@ -28,25 +28,30 @@ class MineField(object):
                 row += ' '
             print(row)
 
-class Mine(object):
+class Mine(MineField):
     def __init__(self, pos_x, pos_y, is_flagged, is_open, has_mine):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.is_flagged = is_flagged
         self.is_open = is_open
         self.has_mine = has_mine
+        self.neighbors = [[self.pos_x + 1, self.pos_y], [self.pos_x + 1, self.pos_y - 1], [self.pos_x, self.pos_y - 1], [self.pos_x - 1, self.pos_y - 1 ], [self.pos_x - 1, self.pos_y], [self.pos_x - 1, self.pos_y + 1], [self.pos_x, self.pos_y + 1], [self.pos_x + 1, self.pos_y + 1]]
+    def AmountOfNeighbors(self, list_of_mines):
+        amount = 0
+        for i in self.neighbors:
+            if list_of_mines[i[0]][i[1]].has_mine == True:
+                amount += 1
+        return amount
     def InitSquare(self):
         kerroin = 10 #muutettava myöhemmmin vastaamaan ikkunan mittoja.
         return [Point(kerroin * self.pos_x, kerroin * self.pos_y), Point(kerroin * self.pos_x + kerroin, kerroin * self.pos_y + kerroin)]
-    def AmountOfNeighbors(self):
-        amount = 0
-        
+
 
 minefield = MineField(16, 16)
 minefield.Initialize()
-
 # for i in minefield.mines:
 #     for a in i:
 #         print(a.has_mine)
 #         print(a.InitSquare())
 minefield.PrintPretty()
+print(minefield.mines[10][5].AmountOfNeighbors(minefield.mines))
